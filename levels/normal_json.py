b8ac0202a17e0f28563393d802fc437846c29767
Colmax06 <collonv.m@gmail.com>
2026-02-10 01:20:13 +0100

    fix lvl.json

--- levels/normal_json.py
+++ levels/normal_json.py
@@ -1,12 +1,14 @@
 import json
 
-with open("levels\\lvl2.json", 'r') as f:
+with open("levels\\lvl1.json", 'r') as f:
     data = json.load(f)
     level_length = data['level_length']
     obstacle_liste = data['obstacles']
     for obstacle in obstacle_liste:
         if 'used' in obstacle:
             del obstacle['used']
+        if 'turned' in obstacle:
+            del obstacle['turned']
 
 with open("normal.json", 'w') as f:
     data = {"level_length": level_length, "obstacles": obstacle_liste}
